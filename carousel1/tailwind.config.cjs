88a492be84f68f984a32e6f065416e8c9a9bd05a
a1x02 <Sashurik2002@gmail.com>
2023-07-24 23:14:09 +0300

    feat: реализовать базовую часть скроллера

---
+++ carousel1/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{svelte,html,js}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'arrow-left': "url('src/img/icon-arrow-left.svg')",
+        'arrow-right': "url('src/img/icon-arrow-right.svg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
